5dc7f109b6c420c4f864931421f5d47022bbf53f
David Rodríguez <deivid.rodriguez@riseup.net>
2020-07-23 21:43:20 +0200

    Use whatever `rake` comes with ruby
    We don't really need to lock `rake`, and we're using whatever `rake`
    comes with ruby anyways when we run `rake setup` in CI or our local
    machine.

--- dev_gems.rb
+++ dev_gems.rb
@@ -1,7 +1,6 @@
 source "https://rubygems.org"
 
 gem "rdoc", "6.2.0" # 6.2.1 is required > Ruby 2.3
-gem "rake", "~> 13.0"
 gem "minitest", "~> 5.13"
 gem "simplecov", "~> 0.17.0"
 gem "rubocop", "~> 0.80.1"
